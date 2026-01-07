6c19c4059b13f8e7448430af88efc2d4f99e2942
David van Oudheusden <doudheusden@novacollege.nl>
2026-01-07 10:54:24 +0100

    Add SQL script to create leerlingen table

---
+++ leerlingen.sql
@@ -0,0 +1,8 @@
+CREATE TABLE leerlingen (
+    id INT PRIMARY KEY,
+    voornaam VARCHAR(50) NOT NULL,
+    achternaam VARCHAR(50) NOT NULL,
+    leerlingnummer INT NOT NULL,
+    klas_id INT NOT NULL,
+);
+
